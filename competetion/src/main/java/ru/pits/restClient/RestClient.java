a894b9f1baa3b6ce9fb039f12f019238af4f28a9
Vlad <vladislav.chernov@qwerty.com>
2017-08-06 16:22:34 +0300

    небольшо жксперимент на примере получения токена

--- competetion/src/main/java/ru/pits/restClient/RestClient.java
+++ competetion/src/main/java/ru/pits/restClient/RestClient.java
@@ -12,12 +12,31 @@ import javax.ws.rs.core.UriBuilder;
 public class RestClient {
     public void execute(){
         Client client = Client.create();
+        String url = "/ps/auth/api/token";
+        //Протокол я так понимаю, что в настройках где-то.
+
         WebResource resource = client.resource("http://localhost:10102").path("/rest");
 
+        //nтипа как для токена получиь вбил хэдер
         WebResource.Builder header = resource.header("Content-Type", "application/x-www-form-urlencoded")
-                .header("Accept-Encoding", "gzip, deflate");
+                .header("Accept-Encoding", "gzip, deflate")
+                .header("Host", "vlg-sso-lb1a.megafon.ru:47132")
+                .header("Accept", "application/json")
+                .header("Content-Length", "56")
+                .header("Authorization", "Basic {HASH}");
+        //А вот это дело из Map<String, String> построить как?
+
+        //Тело для поста предлагают делать так "nj djj,ot dthyj
+        /*
+        MultivaluedMap formData = new MultivaluedMapImpl();
+        formData.add("name1", "val1");
+        formData.add("name2", "val2");
+        */
+
         header.post();
+        //неясно куда вывалится ответ.
 
 
     }
 }
+5
