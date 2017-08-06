00d433af5a0d364e56dc65126b334bcac2fc7cc0
Vlad <vladislav.chernov@qwerty.com>
2017-08-06 18:54:45 +0300

    написан класс GetToken

--- competetion/src/test/java/ru/pits/SmokeTest.java
+++ competetion/src/test/java/ru/pits/SmokeTest.java
@@ -1,5 +1,13 @@
 package ru.pits;
 
-//Собсвенно наш SmokeTest
+import org.testng.annotations.Test;
+
+/**C266394: [Smoke] Добавление пакета услуг: Подключение бесплатного пакета без срока действия
+ (время абонента = времени БД)*/
 public class SmokeTest {
+    @Test
+    public void testExecute() {
+
+    }
+
 }
