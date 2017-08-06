fc4bb71577abc1d86d49ea9d33a28604c6551602
Vlad <vladislav.chernov@qwerty.com>
2017-08-06 18:33:36 +0300

    написан класс GetToken

--- competetion/src/main/java/ru/pits/keywords/GettingToken.java
+++ competetion/src/main/java/ru/pits/keywords/GettingToken.java
@@ -1,4 +1,57 @@
 package competetion.src.main.java.ru.pits.keywords;
 
+import ru.pits.restClient.RestClient;
+import ru.pits.restClient.RestRequest;
+
+import javax.ws.rs.core.MultivaluedMap;
+import java.util.HashMap;
+import java.util.Map;
+
 public class GettingToken {
+
+    /**Входные параметры*/
+    String hash = 'U1NPX0hBUzpTU09fSEFT';
+    String login = 'TEST_SBMS';
+    String password = '*********';
+
+    /**Выходные параметры*/
+    String token;
+
+    /**параметры для http запроса*/
+    Map<String, String > headersMap = new HashMap<>();
+    MultivaluedMap requestBody;);
+
+    public GettingToken() {
+        //Заполняем хэдеры
+        headersMap.put("url", "/ps/auth/api/token");
+        headersMap.put("Content-Type", "application/x-www-form-urlencoded");
+        headersMap.put("Accept-Encoding", "gzip, deflate");
+        headersMap.put("Host", "vlg-sso-lb1a.megafon.ru:47132");
+        headersMap.put("Accept", "application/json");
+        headersMap.put("Content-Length", "56");
+        headersMap.put("Authorization", "Basic " + this.hash);
+
+        //Зполняем тело
+        requestBody.add("grant_type", "password&");
+        requestBody.add("username", this.login);
+        requestBody.add("password", this.password);
+
+    }
+
+
+    public String getToken() {
+        RestClient rc = new RestClient();
+        RestRequest rr = new RestRequest();
+        rr.setHeadersMap(this.headersMap);
+        rr.setRequest(this.requestBody);
+        token = rc.execute(rr)
+        return token;
+    }
+
+/*
+    String requestBody = "grant_type=password&" +
+            "username=" + this.login +
+            "&password=" + this.password;
+*/
+
 }
--- competetion/src/main/java/ru/pits/restClient/RestClient.java
+++ competetion/src/main/java/ru/pits/restClient/RestClient.java
@@ -10,7 +10,6 @@ public class RestClient {
     public RestResponse execute(RestRequest request){
         Client client = Client.create();
         String url = "/ps/auth/api/token";
-        //Протокол я так понимаю, что в настройках где-то.
 
         WebResource resource = client.resource("http://localhost:10102").path("/rest");
 
@@ -21,31 +20,14 @@ public class RestClient {
                     headersMapEntry.getKey(), headersMapEntry.getValue());
         }
 
-        //nтипа как для токена получиь вбил хэдер
-//        WebResource.Builder header = resource.header("Content-Type", "application/x-www-form-urlencoded")
-//                .header("Accept-Encoding", "gzip, deflate")
-//                .header("Host", "vlg-sso-lb1a.megafon.ru:47132")
-//                .header("Accept", request.getAccept())
-//                .header("Content-Length", "56")
-//                .header("Authorization", "Basic {HASH}");
-        //А вот это дело из Map<String, String> построить как?
-
-        //Тело для поста предлагают делать так "nj djj,ot dthyj
-        /*
-        MultivaluedMap formData = new MultivaluedMapImpl();
-        formData.add("name1", "val1");
-        formData.add("name2", "val2");
-        */
-
 
         RestResponse response = builder
                 .type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)
                 .post(RestResponse.class, request.getRequest());
 
         System.out.println(response);
-        //неясно куда вывалится ответ.
-
 
         return response;
     }
 }
+
